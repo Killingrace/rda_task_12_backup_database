16a04b2a96f889b5c21f6f55244d03bda205ee87
Dmytro <dima.shutko5@gmail.com>
2026-04-10 18:54:29 +0200

    solution 1

--- task.sh
+++ task.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+DATENOW=$(date | tr " " "-")
+FULL="ShopDB-"${DATENOW:0:19}
+FULL_BACKUP=$FULL"-backup.sql"
+DATA_BACKUP=$FULL"-data-backup.sql"
+
+mysqldump -u $DB_USER -p$DB_PASSWORD "ShopDB" > $FULL_BACKUP
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info $DB_NAME > $DATA_BACKUP
+
+mysql -u $DB_USER -p$DB_PASSWORD "ShopDBReserve" < $FULL_BACKUP
+
+mysql -u $DB_USER -p$DB_PASSWORD "ShopDBDevelopment" < $DATA_BACKUP
