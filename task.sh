f2e8532cf8496f7f16d944a72366e6907e782d07
Dmytro <dima.shutko5@gmail.com>
2026-04-10 19:27:01 +0200

    solution3

--- task.sh
+++ task.sh
@@ -1,15 +1,15 @@
-FULL="ShopDB-"$(date +"%Y-%m-%d_%H-%M-%S" | tr " " "-")
+FULL="ShopDB-"$(date +"%Y-%m-%d_%H-%M-%S")
 FULL_BACKUP=$FULL"-backup.sql"
 DATA_BACKUP=$FULL"-data-backup.sql"
 
-if [[ -n "$DB_USER" || -n "$DB_PASSWORD" ]]; then
-    mysqldump -u $DB_USER -p$DB_PASSWORD "ShopDB" > $FULL_BACKUP
+if [[ -n "$DB_USER" && -n "$DB_PASSWORD" ]]; then
+    mysqldump -u "$DB_USER" -p"$DB_PASSWORD" "ShopDB" > "$FULL_BACKUP"
 
-    mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info "ShopDB" > $DATA_BACKUP
+    mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info "ShopDB" > "$DATA_BACKUP"
 
-    mysql -u $DB_USER -p$DB_PASSWORD "ShopDBReserve" < $FULL_BACKUP
-
-    mysql -u $DB_USER -p$DB_PASSWORD "ShopDBDevelopment" < $DATA_BACKUP
+    mysql -u "$DB_USER" -p"$DB_PASSWORD" "ShopDBReserve" < "$FULL_BACKUP
+"
+    mysql -u "$DB_USER" -p"$DB_PASSWORD" "ShopDBDevelopment" < "$DATA_BACKUP"
 else
     echo "Provide DB_PASSWORD and DB_USER"
 fi
