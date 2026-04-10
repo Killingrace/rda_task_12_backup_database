76a80936e086b0811f3e23d4e71065a4ddb658b0
Dmytro <dima.shutko5@gmail.com>
2026-04-10 19:18:12 +0200

    solution2

--- task.sh
+++ task.sh
@@ -1,13 +1,15 @@
-#! /bin/bash
-DATENOW=$(date | tr " " "-")
-FULL="ShopDB-"${DATENOW:0:19}
+FULL="ShopDB-"$(date +"%Y-%m-%d_%H-%M-%S" | tr " " "-")
 FULL_BACKUP=$FULL"-backup.sql"
 DATA_BACKUP=$FULL"-data-backup.sql"
 
-mysqldump -u $DB_USER -p$DB_PASSWORD "ShopDB" > $FULL_BACKUP
+if [[ -n "$DB_USER" || -n "$DB_PASSWORD" ]]; then
+    mysqldump -u $DB_USER -p$DB_PASSWORD "ShopDB" > $FULL_BACKUP
 
-mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info $DB_NAME > $DATA_BACKUP
+    mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info "ShopDB" > $DATA_BACKUP
 
-mysql -u $DB_USER -p$DB_PASSWORD "ShopDBReserve" < $FULL_BACKUP
+    mysql -u $DB_USER -p$DB_PASSWORD "ShopDBReserve" < $FULL_BACKUP
 
-mysql -u $DB_USER -p$DB_PASSWORD "ShopDBDevelopment" < $DATA_BACKUP
+    mysql -u $DB_USER -p$DB_PASSWORD "ShopDBDevelopment" < $DATA_BACKUP
+else
+    echo "Provide DB_PASSWORD and DB_USER"
+fi
